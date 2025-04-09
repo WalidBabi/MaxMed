4f25c17ebccdc813ad55a6ae90644b34318ad88d
Walid Babi <walid.babi.du@gmail.com>
2025-04-09 21:21:30 +0400

    shopping cart accessibility mobile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -101,7 +101,7 @@
 
         <!-- Mobile menu button -->
         <div class="md:hidden flex items-center justify-between px-4 py-2 border-t border-gray-100">
-            <a href="{{ route('cart.view') }}" class="flex items-center text-gray-600">
+            <a href="{{ route('cart.view') }}" class="flex items-center text-gray-600" aria-label="Shopping cart">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
@@ -110,6 +110,7 @@
                     {{ $cartQuantity }}
                 </span>
                 @endif
+                <span class="sr-only">View shopping cart</span>
             </a>
             <button @click="open = !open" class="text-gray-500 hover:text-gray-600 focus:outline-none" aria-label="Toggle navigation menu">
                 <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
