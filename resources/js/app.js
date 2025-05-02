2e29f8fa4ab331609f2081616cef3e1cb648db69
Walid Babi <walid.babi.du@gmail.com>
2025-05-02 22:59:15 +0400

    remove loader

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,7 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import loadingScreen from './components/LoadingScreen';
-import './navigation-loader';
 
-// Make Alpine available globally
 window.Alpine = Alpine;
 
-// Register the loadingScreen component with Alpine
-Alpine.data('loadingScreen', loadingScreen);
-
-// Start Alpine
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Starting Alpine.js');
-    Alpine.start();
-});
+Alpine.start();
