71e534a0e440177ef7a7b30ca39a86eeb191eaef
Walid Babi <walid.babi.du@gmail.com>
2025-07-08 11:21:26 +0400

    fix: update admin route middleware to use AdminMiddleware for improved access control

--- routes/web.php
+++ routes/web.php
@@ -358,7 +358,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
     // Admin Routes
-    Route::middleware(['web', 'auth', 'verified', 'admin'])->name('admin.')->prefix('admin')->group(function () {
+    Route::middleware(['web', 'auth', 'verified', \App\Http\Middleware\AdminMiddleware::class])->name('admin.')->prefix('admin')->group(function () {
         Route::get('/dashboard', fn() => view('admin.dashboard'));
 
         // Inquiry Management
